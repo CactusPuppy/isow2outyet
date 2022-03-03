58a65bae543482c7efc210c8590b89c515129eec
CactusPuppy <puppycactus@gmail.com>
2022-03-02 20:18:51 -0800

    fix: Accessibility issues with contrast

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,7 @@
     <h1 class="text-6xl font-display font-medium dark:text-slate-100">{isOW2Out ? "Yes." : "No."}</h1>
   </div>
   <div class="flex justify-between my-4 mx-2 px-4 h-8 w-full">
-    <p class="text-xs text-gray-500 dark:text-slate-400 italic text-left">Created by <a class="text-blue-500 dark:text-blue-300 underline" href="https://twitter.com/Cactus_Puppy" rel="noreferrer noopener" target="_blank">@Cactus_Puppy</a><br /><a href="https://github.com/CactusPuppy/isow2outyet" rel="noreferrer noopener" target="_blank" class="text-blue-500 dark:text-blue-300 underline">GitHub</a></p>
+    <p class="text-xs text-gray-500 dark:text-slate-400 italic text-left">Created by <a class="text-blue-600 dark:text-blue-300 underline" href="https://twitter.com/Cactus_Puppy" rel="noreferrer noopener" target="_blank">@Cactus_Puppy</a><br /><a href="https://github.com/CactusPuppy/isow2outyet" rel="noreferrer noopener" target="_blank" class="text-blue-600 dark:text-blue-300 underline">GitHub</a></p>
     <p class="text-xs text-gray-500 dark:text-slate-400 italic text-right">
       This site and its creator are not affiliated with Overwatch or Blizzard Entertainment.
       <br />Overwatch 2 and the Overwatch 2 logo are ©2022 Blizzard Entertainment, Inc.
