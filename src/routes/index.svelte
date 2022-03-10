6e069c29db52978426438a3664425ffef5dc241f
CactusPuppy <puppycactus@gmail.com>
2022-03-10 11:38:32 -0800

    feat: Update to include OW2 news

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,14 +2,28 @@
   import {formatDistanceToNowStrict, format} from "date-fns";
 
   export let ow2ReleaseDate = new Date(2077, 4, 20);
+  // export let ow2BetaDate = new Date(2022, 4, 31);
   $: isOW2Out = new Date(Date.now()) > ow2ReleaseDate;
   let releaseDateAnnounced = false;
 </script>
 
 <div class="flex flex-col justify-center items-center h-[100vh] w-[100vw]">
   <div class="flex-grow flex flex-col justify-center items-center py-8">
-    <h1 class="text-6xl font-display font-medium dark:text-slate-100">{isOW2Out ? "Yes." : "No."}</h1>
-    <p class="my-4 mx-2 italic dark:text-slate-100 text-center">{releaseDateAnnounced ? `${formatDistanceToNowStrict(ow2ReleaseDate)} until release on ${format(ow2ReleaseDate, "MMMM do, yyyy")}` : "...and no release date has been announced yet."}</p>
+    <h1 class="text-6xl font-display font-medium dark:text-slate-100">{isOW2Out ? "Yes." : "Not yet."}</h1>
+    <p class="my-4 mx-2 italic dark:text-slate-100 text-center">
+      {#if releaseDateAnnounced}
+        {formatDistanceToNowStrict(ow2ReleaseDate)} until release on {format(ow2ReleaseDate, "MMMM do, yyyy")}
+      {:else}
+        ...but there is a beta coming in late April.<br />
+        <a href="https://playoverwatch.com/beta"
+          rel="noreferrer noopener"
+          target="_blank"
+          class="text-blue-600 dark:text-blue-300 underline"
+        >
+          Sign up here
+        </a>.
+      {/if}
+    </p>
   </div>
   <div class="flex justify-between my-4 mx-2 px-4 h-8 w-full">
     <p class="text-xs text-gray-500 dark:text-slate-400 italic text-left">Created by <a class="text-blue-600 dark:text-blue-300 underline" href="https://twitter.com/Cactus_Puppy" rel="noreferrer noopener" target="_blank">@Cactus_Puppy</a><br /><a href="https://github.com/CactusPuppy/isow2outyet" rel="noreferrer noopener" target="_blank" class="text-blue-600 dark:text-blue-300 underline">GitHub</a></p>
