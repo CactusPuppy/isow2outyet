69a73b61f3a94fd8d352b5b1320a4263b10f95e1
CactusPuppy <puppycactus@gmail.com>
2022-05-23 15:35:00 -0700

    feat: Font tweaks

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,15 +9,16 @@
 
 <div class="flex flex-col justify-center items-center h-[100vh] w-[100vw]">
   <div class="flex-grow flex flex-col justify-center items-center py-8">
-    <h1 class="text-6xl font-display font-bold dark:text-slate-100">{isOW2Out ? "Yes." : "Not yet."}</h1>
-    <p class="my-4 mx-2 italic dark:text-slate-100 text-center">
+    <h1 class="text-7xl font-display font-semibold dark:text-slate-100">{isOW2Out ? "Yes." : "Not yet."}</h1>
+    <p class="my-4 mx-2 text- italic dark:text-slate-100 text-center">
       {#if releaseDateAnnounced}
-        {formatDistanceToNowStrict(ow2ReleaseDate)} until release on {format(ow2ReleaseDate, "MMMM do, yyyy")}
+        <span class="text-ow2-orange dark:text-ow2-light-orange">{formatDistanceToNowStrict(ow2ReleaseDate)} </span>
+        until release on <span class="text-ow2-orange dark:text-ow2-light-orange">{format(ow2ReleaseDate, "MMMM do, yyyy")}</span>
       {/if}
     </p>
   </div>
   <div class="flex justify-between my-4 mx-2 px-4 h-8 w-full">
-    <p class="text-xs text-gray-500 dark:text-slate-400 italic text-left">Created by <a class="text-blue-600 dark:text-blue-300 underline" href="https://twitter.com/Cactus_Puppy" rel="noreferrer noopener" target="_blank">@Cactus_Puppy</a><br /><a href="https://github.com/CactusPuppy/isow2outyet" rel="noreferrer noopener" target="_blank" class="text-blue-600 dark:text-blue-300 underline">GitHub</a></p>
+    <p class="text-xs text-gray-500 dark:text-slate-400 italic text-left">Created by <a class="text-ow2-orange dark:text-ow2-light-orange underline" href="https://twitter.com/Cactus_Puppy" rel="noreferrer noopener" target="_blank">@Cactus_Puppy</a><br /><a href="https://github.com/CactusPuppy/isow2outyet" rel="noreferrer noopener" target="_blank" class="text-ow2-orange dark:text-ow2-light-orange underline">GitHub</a></p>
     <p class="text-xs text-gray-500 dark:text-slate-400 italic text-right">
       This site and its creator are not affiliated with Overwatch or Blizzard Entertainment.
       <br />Overwatch 2 and the Overwatch 2 logo are ©2022 Blizzard Entertainment, Inc.
